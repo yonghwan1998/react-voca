9eb4b5e35a91f748a885b8b74be778cb77604f25
yonghwan1998 <bbl737898@gmail.com>
2024-11-13 14:16:24 +0900

    [React.js] 컴포넌트 학습

---
+++ src/component/Welcome.js
@@ -0,0 +1,7 @@
+export default function Welcome() {
+    return (
+        <h2>
+            Welcome
+        </h2>
+    );
+};
---
+++ src/component/World.js
@@ -0,0 +1,7 @@
+export default function World() {
+    return(
+        <h3>
+            World!
+        </h3>
+    );
+};
