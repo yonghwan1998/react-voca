83ce31b4ae95e6d653c4689fc5b76efb1109e744
yonghwan1998 <bbl737898@gmail.com>
2024-11-13 19:14:36 +0900

    [React.js] Loading 구현

--- src/component/Day.js
+++ src/component/Day.js
@@ -4,12 +4,12 @@ import useFetch from "../hooks/useFetch";
 
 export default function Day() {
     const {day} = useParams();
-
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
     return (
         <>
             <h2>Day {day}</h2>
+            {words.length === 0 && <span>Loading...</span>}
             <table>
                 <tbody>
                     {words.map(word => (
