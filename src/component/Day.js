8699a38af99a493c8377b499b4251d96f08a058d
yonghwan1998 <bbl737898@gmail.com>
2024-11-13 15:54:53 +0900

    [React.js] map() 반복문

---
+++ src/component/Day.js
@@ -0,0 +1,27 @@
+import dummy from "../db/data.json"
+
+export default function Day() {
+
+    const day = 3;
+    const wordList = dummy.words.filter(word => (
+        word.day === day
+    ));
+    return (
+        <>
+            <table>
+                <tbody>
+                    {wordList.map(word => (
+                        <tr key={word.id}>
+                            <td>
+                                {word.eng}
+                            </td>
+                            <td>
+                                {word.kor}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+}
--- src/component/World.js
+++
@@ -1,7 +0,0 @@
-export default function World() {
-    return(
-        <h3>
-            World!
-        </h3>
-    );
-}
